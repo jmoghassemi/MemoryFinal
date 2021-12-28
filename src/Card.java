e08f99a4f2e15665f715db7391f5639676723620
hadi-tghl <hadi@gmx.se>
2021-12-28 22:18:20 +0100

    Add some code

--- src/Card.java
+++ src/Card.java
@@ -1,17 +1,16 @@
 // this is the Card class for this memory game, can be changed as desired
-// can also include picture for game interfaces
-// this class can also be extended during development
-// demonstration how look like code and is not written by the groups' member. stackoverflow
 // it's should have a method to save and show the images on every row
 
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Card extends JButton {
     private boolean cardMatched;
     private boolean cardSelected;
 
     private String back;
+    private int temp;
 
     public Card(String back) {
         this.back = back;
@@ -44,6 +43,26 @@ public class Card extends JButton {
             return true;
         }else {
             return false;
+        }
+    }
+    public void setColor(Graphics g) {
+        switch (temp) {
+            case 0: g.setColor(Color.RED);
+            break;
+            case 1: g.setColor(Color.GREEN);
+            break;
+            case 2: g.setColor(Color.BLUE);
+            break;
+            case 3: g.setColor(Color.ORANGE);
+            break;
+            case 4: g.setColor(Color.CYAN);
+            break;
+            case 5: g.setColor(Color.MAGENTA);
+            break;
+            case 6: g.setColor(Color.PINK);
+            break;
+            case 7: g.setColor(Color.YELLOW);
+            break;
         }
     }
 }
